0c1e4a6cb48a8f77f971a39f08bb3775297fb039
Maharsh Patel <patel@maharsh.net>
2017-12-28 00:51:49 -0700

    autocomplete working

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -13,7 +13,9 @@ $.ajax({
     },
 });
 
-function getWeather(input){
+function getCities(){
+    var input = $("#addressBarField").val();
+
     if (input == ""){
         return;
     }
@@ -38,25 +40,93 @@ function getWeather(input){
                 console.log(response);
                 localStorage.setItem(letter, JSON.stringify(response));
 
-                getWeatherHelper(input, JSON.stringify(response));
+                return(getCitiesHelper(input, JSON.stringify(response)));
             },
         });
 
     }else{
         console.log("Loading From Local Storage");
-        getWeatherHelper(input, localStorage.getItem(letter));
+        return(getCitiesHelper(input, localStorage.getItem(letter)));
     }
 }
 
-function getWeatherHelper(input, citiesList){
+function getCitiesHelper(input, citiesList){
     // go through the citiesList and return all that match
     console.log("Searching...")
 
+    var count = 0;
+
     var result = $.parseJSON(citiesList);
+    var suggestions = [];
     $.each(result, function(k, v) {
         if (k.toLowerCase().indexOf(input.toLowerCase()) != -1){
-            console.log(k, v);
+
+            if (count > 20){
+                return;
+            }
+
+            suggestions.push({"value":k, "data":v});
+            count += 1;
         }
     });
 
+    return suggestions;
+
 }
+
+$('#cityInput').autocomplete({
+    lookup: function (query, done) {
+        // Do Ajax call or lookup locally, when done,
+        // call the callback and pass your results:
+        // var result = {
+        //     suggestions: getCities()
+        // };
+
+        var input = $("#cityInput").val();
+
+        if (input == ""){
+            return;
+        }
+
+        var letter = input[0].toLowerCase()
+
+        if (localStorage.getItem(letter) == null){
+            // load the file from the server for autocomplete and store in localstorage
+            console.log("Loading From Server");
+
+            var formdata = new FormData();
+            formdata.append("letter", letter);
+
+            $.ajax({
+                type: 'POST',
+                url: '/getCities/',
+                data: formdata,
+                contentType: false,
+                processData: false,
+                success: function(data) {
+                    var response = JSON.parse(data);
+                    console.log(response);
+                    localStorage.setItem(letter, JSON.stringify(response));
+
+                    var result = {
+                        suggestions: getCitiesHelper(input, JSON.stringify(response))
+                    };
+                    done(result);
+                    // return();
+                },
+            });
+
+        }else{
+            console.log("Loading From Local Storage");
+            var result = {
+                suggestions: getCitiesHelper(input, localStorage.getItem(letter))
+            };
+            done(result);
+        }
+
+
+    },
+    onSelect: function (suggestion) {
+        console.log('You selected: ' + suggestion.value + ', ' + suggestion.data);
+    }
+});

5c7cc27983e4df50c50590e50784e3b708e0b24f
Maharsh Patel <patel@maharsh.net>
2017-12-27 17:29:32 -0700

    weather by id working

--- testing.go
+++ testing.go
@@ -6,11 +6,42 @@ import (
 	owm "github.com/briandowns/openweathermap"
 
 	"os"
+	"encoding/json"
 )
 
-type Location struct{
-	longitude float64
-	latitude float64
+type Weather struct{
+	//city string
+	Id int `json:"id"`
+	Longitude float64 `json:"longitude"`
+	Latitude float64 `json:"latitude"`
+	Temp float64 `json:"temp"`
+	MinTemp float64 `json:"minTemp"`
+	MaxTemp float64 `json:"maxTemp"`
+	Description string `json:"description"`
+	WindSpeed float64 `json:"windSpeed"`
+	WindDirection float64 `json:"windDirection"`
+	Humidity int `json:"humidity"`
+	Pressure float64 `json:"pressure"`
+	Sunrise int `json:"sunrise"`
+	Sunset int `json:"sunset"`
+	Time int `json:"time"`
+}
+
+func getWeather(cityId int){
+	w, err := owm.NewCurrent("C", "en", os.Getenv("OWM_API_KEY")) // fahrenheit (imperial) with Russian output
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	w.CurrentByID(cityId)
+
+	weather := Weather{cityId, w.GeoPos.Longitude, w.GeoPos.Latitude, w.Main.Temp,
+	w.Main.TempMin, w.Main.TempMax, w.Weather[0].Description, w.Wind.Speed, w.Wind.Deg,
+	w.Main.Humidity, w.Main.Pressure, w.Sys.Sunrise, w.Sys.Sunset, w.Dt}
+
+	weatherMarshalled, _ := json.Marshal(weather)
+	fmt.Printf("%s\n", weatherMarshalled)
+	
 }
 
 func main() {
@@ -18,15 +49,10 @@ func main() {
 
 
 	fmt.Println(os.Getenv("OWM_API_KEY"))
-	w, err := owm.NewCurrent("C", "en", os.Getenv("OWM_API_KEY")) // fahrenheit (imperial) with Russian output
-	if err != nil {
-		log.Fatalln(err)
-	}
 
-	w.CurrentByName("London")
+	getWeather(2172797)
 
 
-	//location := Location{w.GeoPos.Longitude, w.GeoPos.Latitude}
 
-	fmt.Println(w)
+	//fmt.Println(w)
 }
